0f687acd28d36f13a6dba37829526edd2283f3be
Ecce <ecollier871@outlook.com>
2023-03-04 03:00:08 +0000

    somethings off with xray and backend

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -28,7 +28,6 @@ from aws_xray_sdk.core import xray_recorder
 from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
 
 
-
 # HoneyComb - Initialize tracing and an exporter that can send data to Honeycomb
 provider = TracerProvider()
 processor = BatchSpanProcessor(OTLPSpanExporter())
@@ -39,9 +38,10 @@ tracer = trace.get_tracer(__name__)
 # X-ray to start a new recorder
 xray_url = os.getenv("AWS_XRAY_URL")
 xray_recorder.configure(service='backend-flask', dynamic_naming=xray_url)
-XRayMiddleware(app, xray_recorder)
 
 app = Flask(__name__)
+# X ray
+XRayMiddleware(app, xray_recorder)
 
 # HoneyComb - Initialize automatic instrumentation with Flask
 FlaskInstrumentor().instrument_app(app)
