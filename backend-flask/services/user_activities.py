594684d3ad73c63cac8eb79f0ac86c0bc8b72bdb
Ecce <ecollier871@outlook.com>
2023-03-05 02:31:06 +0000

    whoops

--- backend-flask/services/user_activities.py
+++ backend-flask/services/user_activities.py
@@ -4,7 +4,9 @@ from aws_xray_sdk.core import xray_recorder
 
 class UserActivities:
     def run(user_handle):
-        #try:
+        try:
+          
+
             model = {
                 'errors': None,
                 'data': None
@@ -25,15 +27,15 @@ class UserActivities:
                 }]
                 model['data'] = results
 
-            #subsegment = xray_recorder.begin_subsegment('mock-data')
+            subsegment = xray_recorder.begin_subsegment('mock-data')
             # xray -
-          #   dict = {
-          #       "now": now.isoformat(),
-          #       "results-size": len(model['data'])
-          #   }
-          #  # subsegment.put_metadata('key', dict, 'namespace')
-           # xray_recorder.end_subsegment()
-        # finally:
-        #     # close the segment
-        #     xray_recorder.end_subsegment()
+            dict = {
+                "now": now.isoformat(),
+                "results-size": len(model['data'])
+            }
+            subsegment.put_metadata('key', dict, 'namespace')
+            xray_recorder.end_subsegment()
+        finally:
+            # close the segment
+            xray_recorder.end_subsegment()
             return model
