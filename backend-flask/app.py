a528134b05d58c6d5ac459cb171a14a85c6a7003
Ecce <ecollier871@outlook.com>
2023-03-16 01:03:33 +0000

    query is working

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -166,7 +166,7 @@ def data_create_message():
 
 
 @app.route("/api/activities/home", methods=['GET'])
-# @xray_recorder.capture('activities_home')
+@xray_recorder.capture('activities_home')
 def data_home():
     data = HomeActivities.run()
     access_token = extract_access_token(request.headers)
@@ -191,7 +191,7 @@ def data_notifications():
 
 
 @app.route("/api/activities/@<string:handle>", methods=['GET'])
-# @xray_recorder.capture('activities_users')
+@xray_recorder.capture('activities_users')
 def data_handle(handle):
     model = UserActivities.run(handle)
     if model['errors'] is not None:
@@ -226,7 +226,7 @@ def data_activities():
 
 
 @app.route("/api/activities/<string:activity_uuid>", methods=['GET'])
-# @xray_recorder.capture('activities_show')
+@xray_recorder.capture('activities_show')
 def data_show_activity(activity_uuid):
     data = ShowActivity.run(activity_uuid=activity_uuid)
     return data, 200
