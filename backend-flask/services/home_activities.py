2a701ae0ae2153c722097cc6a7f1f70d8d8a22c1
Ecce <ecollier871@outlook.com>
2023-03-20 20:40:38 +0000

    copy pasted to assist with debugging, post are 200s but not adding to db

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -1,44 +1,17 @@
 from datetime import datetime, timedelta, timezone
+from opentelemetry import trace
+
+from lib.db import db
+
+#tracer = trace.get_tracer("home.activities")
+
 class HomeActivities:
-  def run():
-    now = datetime.now(timezone.utc).astimezone()
-    results = [{
-      'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
-      'handle':  'E_coco',
-      'message': 'Cloud is so much more interesting and fun than web development',
-      'created_at': (now - timedelta(days=2)).isoformat(),
-      'expires_at': (now + timedelta(days=5)).isoformat(),
-      'likes_count': 5,
-      'replies_count': 1,
-      'reposts_count': 0,
-      'replies': [{
-        'uuid': '26e12864-1c26-5c3a-9658-97a10f8fea67',
-        'reply_to_activity_uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
-        'handle':  'CloudsN_myCoffee',
-        'message': 'Wait until you get into cloudformation, heaven!',
-        'likes_count': 0,
-        'replies_count': 0,
-        'reposts_count': 0,
-        'created_at': (now - timedelta(days=2)).isoformat()
-      }],
-    },
-    {
-      'uuid': '66e12864-8c26-4c3a-9658-95a10f8fea67',
-      'handle':  'Duke Ellington',
-      'message': 'This new generation music is trash. Play an instrument, I dare you. ',
-      'created_at': (now - timedelta(days=7)).isoformat(),
-      'expires_at': (now + timedelta(days=9)).isoformat(),
-      'likes': 0,
-      'replies': []
-    },
-    {
-      'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
-      'handle':  'Michael Jackson',
-      'message': 'Heal the world...no, for real. Pronto',
-      'created_at': (now - timedelta(hours=1)).isoformat(),
-      'expires_at': (now + timedelta(hours=12)).isoformat(),
-      'likes': 0,
-      'replies': []
-    }
-    ]
+  def run(cognito_user_id=None):
+    #logger.info("HomeActivities")
+    #with tracer.start_as_current_span("home-activites-mock-data"):
+    #  span = trace.get_current_span()
+    #  now = datetime.now(timezone.utc).astimezone()
+    #  span.set_attribute("app.now", now.isoformat())
+    sql = db.template('activities','home')
+    results = db.query_array_json(sql)
     return results
