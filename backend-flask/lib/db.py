98456186c6aad22adeaf49295a00177d06aa9ca6
Ecce <ecollier871@outlook.com>
2023-03-31 21:58:57 +0000

    no idea

--- backend-flask/lib/db.py
+++ backend-flask/lib/db.py
@@ -11,7 +11,6 @@ class Db:
   def template(self,*args):
     pathing = list((app.root_path,'db','sql',) + args)
     pathing[-1] = pathing[-1] + ".sql"
-
     template_path = os.path.join(*pathing)
 
     green = '\033[92m'
@@ -24,8 +23,11 @@ class Db:
     return template_content
 
   def init_pool(self):
-    connection_url = os.getenv("CONNECTION_URL")
-    self.pool = ConnectionPool(connection_url)
+  #   connection_url = os.getenv("CONNECTION_URL")
+  #   self.pool = ConnectionPool(connection_url)
+   return self
+
+
   # we want to commit data such as an insert
   # be sure to check for RETURNING in all uppercases
   def print_params(self,params):
@@ -40,6 +42,8 @@ class Db:
     no_color = '\033[0m'
     print(f'{cyan} SQL STATEMENT-[{title}]------{no_color}')
     print(sql,params)
+
+
   def query_commit(self,sql,params={}):
     self.print_sql('commit with returning',sql,params)
 
@@ -57,6 +61,8 @@ class Db:
           return returning_id
     except Exception as err:
       self.print_sql_err(err)
+
+
   # when we want to return a json object
   def query_array_json(self,sql,params={}):
     self.print_sql('array',sql,params)
@@ -67,6 +73,8 @@ class Db:
         cur.execute(wrapped_sql,params)
         json = cur.fetchone()
         return json[0]
+
+
   # When we want to return an array of json objects
   def query_object_json(self,sql,params={}):
 
@@ -82,6 +90,8 @@ class Db:
           "{}"
         else:
           return json[0]
+
+
   def query_value(self,sql,params={}):
     self.print_sql('value',sql,params)
     with self.pool.connection() as conn:
@@ -89,6 +99,8 @@ class Db:
         cur.execute(sql,params)
         json = cur.fetchone()
         return json[0]
+
+
   def query_wrap_object(self,template):
     sql = f"""
     (SELECT COALESCE(row_to_json(object_row),'{{}}'::json) FROM (
@@ -96,6 +108,8 @@ class Db:
     ) object_row);
     """
     return sql
+
+
   def query_wrap_array(self,template):
     sql = f"""
     (SELECT COALESCE(array_to_json(array_agg(row_to_json(array_row))),'[]'::json) FROM (
@@ -103,6 +117,7 @@ class Db:
     ) array_row);
     """
     return sql
+
   def print_sql_err(self,err):
     # get details about the exception
     err_type, err_obj, traceback = sys.exc_info()
