96a1971ea71a526af0c72308cd064f07ebe2286f
Ecce <ecollier871@outlook.com>
2023-05-15 17:49:44 +0000

    debugging attempt

--- frontend-react-js/src/lib/CheckAuth.js
+++ frontend-react-js/src/lib/CheckAuth.js
@@ -1,15 +1,13 @@
 import { Auth } from 'aws-amplify';
 
-export const getAccessToken = async () => {
-  try {
-    Auth.currentSession()
-    .then((cognito_user_session) => {
-      const access_token = cognito_user_session.accessToken.jwtToken
-      localStorage.setItem("access_token", access_token)
-    })    
-  } catch (error) {
-    console.log(error)
-  }
+
+export const getAccessToken= async () => {
+  Auth.currentSession()
+  .then((cognito_user_session) => {
+    const access_token = cognito_user_session.accessToken.jwtToken
+    localStorage.setItem("access_token", access_token)
+  })
+  .catch((err) => console.log(err));
 }
 
 export const checkAuth = async (setUser) => {
--- frontend-react-js/src/pages/HomeFeedPage.js
+++ frontend-react-js/src/pages/HomeFeedPage.js
@@ -34,7 +34,7 @@ const HomeFeedPage = () => {
         console.log(res)
       }
     } catch (err) {
-      console.log(err);
+      console.log(err)
     }
   };
 
