af9eba08db62b76334fa7a997a51275b1884412b
Ecce <ecollier871@outlook.com>
2023-06-09 14:20:48 +0000

    aws amplify configure error fixed, hardcoded env vars

--- frontend-react-js/src/App.js
+++ frontend-react-js/src/App.js
@@ -13,20 +13,38 @@ import React from 'react';
 import { Amplify, Auth } from 'aws-amplify';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
-Amplify.configure({
-  "AWS_PROJECT_REGION": process.env.REACT_APP_AWS_PROJECT_REGION,
-  "aws_cognito_region": process.env.REACT_APP_AWS_COGNITO_REGION,
-  "aws_user_pools_id": process.env.REACT_APP_AWS_USER_POOLS_ID,
-  "aws_user_pools_web_client_id": process.env.REACT_APP_CLIENT_ID,
+const params = {
+  "AWS_PROJECT_REGION": "us-east-1",
+  "aws_cognito_region": "us-east-1",
+  "aws_user_pools_id": "us-east-1_GHUR3Uvsf",
+  "aws_user_pools_web_client_id": "6ueligdf6dsbo7cu7j8kiig1s",
   "oauth": {},
   Auth: {
     // We are not using an Identity Pool
     // identityPoolId: process.env.REACT_APP_IDENTITY_POOL_ID, // REQUIRED - Amazon Cognito Identity Pool ID
-    region: process.env.REACT_APP_AWS_PROJECT_REGION,           // REQUIRED - Amazon Cognito Region
-    userPoolId: process.env.REACT_APP_AWS_USER_POOLS_ID,         // OPTIONAL - Amazon Cognito User Pool ID
-    userPoolWebClientId: process.env.REACT_APP_CLIENT_ID,   // OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
+    region: "us-east-1",           // REQUIRED - Amazon Cognito Region
+    userPoolId: "us-east-1_GHUR3Uvsf",         // OPTIONAL - Amazon Cognito User Pool ID
+    userPoolWebClientId: "6ueligdf6dsbo7cu7j8kiig1s",   // OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
   }
-});
+}
+
+console.log('params', params)
+
+Amplify.configure(params
+ 
+  // "AWS_PROJECT_REGION": process.env.REACT_APP_AWS_PROJECT_REGION,
+  // "aws_cognito_region": process.env.REACT_APP_AWS_COGNITO_REGION,
+  // "aws_user_pools_id": process.env.REACT_APP_AWS_USER_POOLS_ID,
+  // "aws_user_pools_web_client_id": process.env.REACT_APP_CLIENT_ID,
+  // "oauth": {},
+  // Auth: {
+  //   // We are not using an Identity Pool
+  //   // identityPoolId: process.env.REACT_APP_IDENTITY_POOL_ID, // REQUIRED - Amazon Cognito Identity Pool ID
+  //   region: process.env.REACT_APP_AWS_PROJECT_REGION,           // REQUIRED - Amazon Cognito Region
+  //   userPoolId: process.env.REACT_APP_AWS_USER_POOLS_ID,         // OPTIONAL - Amazon Cognito User Pool ID
+  //   userPoolWebClientId: process.env.REACT_APP_CLIENT_ID,   // OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
+  // }
+);
 
 
 const App = () => {
